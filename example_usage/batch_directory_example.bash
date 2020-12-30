94bdca9d178c83786a211e827c180cb4871a918e
Sam <42655251+sbowerma@users.noreply.github.com>
2020-12-30 16:27:00 +0000

    Delete batch_directory_example.bash
    Remove outdated file.

--- example_usage/batch_directory_example.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-image_analyzer.py -batch_dir ./ -m_ext ".nd2" -roi_ext "_ROI.tif" -o precalc-batch_directory_example/ -nchan 'DAPI' -pchan 'EGFP' -x_buff "-10" -y_buff "10" -n_rois 0 -pre_frames 6 --drift_correct --bg_correct
-
